ae8ee27db0c42db17eb29047eae3e82120576cd7
root <ngkaho1234@gmail.com>
2015-12-19 17:16:30 +0000

    Remove useless files under test/ directory.

--- test/blockdev.h
+++
@@ -1,9 +0,0 @@
-#ifndef BLOCKDEV_H
-#define BLOCKDEV_H
-
-#include "lwext4/lwext4/ext4.h"
-
-int blockdev_get(char *fname, struct ext4_blockdev **pbdev);
-void blockdev_put(struct ext4_blockdev *bdev);
-
-#endif
